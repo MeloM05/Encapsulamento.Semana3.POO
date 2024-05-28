108526c09956f918aa9da76fafe82f4975c0db03
Giovana Melo <giovana3107melo@gmail.com>
2024-05-27 22:09:32 -0300

    Criei cód. do exercício
    Crie um algoritmo em Java contendo uma classe chamada Automovel. Ela deve possuir atributos públicos (public), protegidos (protected) e privados (private).

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+import java.lang.classfile.Signature;
+
+/* Crie um algoritmo em Java contendo uma classe chamada Automovel. Ela deve possuir atributos públicos (public), protegidos (protected) e privados (private).
+*/
+public class Automóvel{
+    public String modelo;
+    protected String marca;
+    private String placa;
+}
+
+public Automovel(String modelo, String marca, String placa){
+    this.modelo = modelo;
+    this.marca = marca;
+    this.placa = placa;
+}
+
+public String getPlaca(){
+    return this.placa;
+}
